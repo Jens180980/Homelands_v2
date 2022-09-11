f74825d01ffe82825bb55976fcd29f3ed1261628
Jens Harder <76961272+Jens180980@users.noreply.github.com>
2022-09-11 10:43:15 +0200

    image modal created

--- src/Components/Pages/Details.jsx
+++ src/Components/Pages/Details.jsx
@@ -1,62 +1,79 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import Style from '../../Assets/scss/Details.module.scss'
-import camera from '../../Assets/img/camera.png'
-import geo from '../../Assets/img/geo.png'
-import heart from '../../Assets/img/heart.png'
-import plan from '../../Assets/img/plan.png'
-import employee from '../../Assets/img/employee.png'
+import Style from "../../Assets/scss/Details.module.scss";
+import camera from "../../Assets/img/camera.png";
+import geo from "../../Assets/img/geo.png";
+import heart from "../../Assets/img/heart.png";
+import plan from "../../Assets/img/plan.png";
+import employee from "../../Assets/img/employee.png";
 import { useContext } from "react";
 import { ModalContent } from "../StateManagement/ModalData";
-import { Modal } from '../Partials/Modal'
+import { Modal } from "../Partials/Modal";
 
 export const Details = () => {
-    const { home_id } = useParams(0);
-    const [homeDetails, setHomeDetails] = useState()
-    const { ModalData, setModalData } = useContext(ModalContent)
+  const { home_id } = useParams(0);
+  const [homeDetails, setHomeDetails] = useState();
+  const { ModalData, setModalData } = useContext(ModalContent);
 
-    console.log(ModalData)
+  const open = (component) => {
+    setModalData(true);
+  };
 
-    const open = () => {
-      setModalData(true)
-      console.log(ModalData)
-    }
+  useEffect(() => {
+    const getDetails = async () => {
+      try {
+        const result = await axios.get(
+          `https://api.mediehuset.net/homelands/homes/${home_id}`
+        );
+        setHomeDetails(result.data.item);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getDetails();
+  }, [home_id]);
 
-    useEffect(() => {
-        const getDetails = async () => {
-            try {
-                const result = await axios.get(`https://api.mediehuset.net/homelands/homes/${home_id}`)
-                setHomeDetails(result.data.item);
-            }
-            catch (err) {
-                console.error(err)
-            }
-        }
-        getDetails()
-    }, [home_id])
-
-
-    return (
-      <>
+  return (
+    <>
       <section className={Style.wrapper}>
-          {homeDetails ? 
-            <>
+        {homeDetails ? (
+          <>
             <div className={Style.imgWrapper}>
-              <img src={homeDetails.images[0].filename.large} alt="house" className={Style.hero}></img>
+              <img
+                src={homeDetails.images[0].filename.large}
+                alt="house"
+                className={Style.hero}
+              ></img>
             </div>
             <div className={Style.textWrapper}>
               <section className={Style.top}>
                 <div className={Style.topleft}>
                   <h3>{homeDetails.address}</h3>
                   <p>{homeDetails.zipcode + " " + homeDetails.city}</p>
-                  <p>{homeDetails.type + " |" + homeDetails.floor_space + "m2 |" + homeDetails.num_rooms + " værelser"}</p>
+                  <p>
+                    {homeDetails.type +
+                      " |" +
+                      homeDetails.floor_space +
+                      "m2 |" +
+                      homeDetails.num_rooms +
+                      " værelser"}
+                  </p>
                   <p>Set {homeDetails.num_clicks} gange</p>
                 </div>
-                <div className={Style.topmiddle}><img src={camera} onClick={open} alt="camera" /><img src={plan} alt="plantegning" /><img src={geo} alt="geolokation" /><img src={heart} alt="like" /></div>
+                <div className={Style.topmiddle}>
+                  <img src={camera} onClick={open} alt="camera" />
+                  <img src={plan} alt="plantegning" />
+                  <img src={geo} alt="geolokation" />
+                  <img src={heart} alt="like" />
+                </div>
                 <div className={Style.topright}>
-                  <p className={Style.priceTxt}>Kontantpris <span>{homeDetails.price} DKK</span></p>
-                  <p className={Style.downpay}>Udbetaling {homeDetails.payout} DKK</p>
+                  <p className={Style.priceTxt}>
+                    Kontantpris <span>{homeDetails.price} DKK</span>
+                  </p>
+                  <p className={Style.downpay}>
+                    Udbetaling {homeDetails.payout} DKK
+                  </p>
                   <p>Ejerudgift per måned {homeDetails.cost} DKK</p>
                 </div>
               </section>
@@ -75,15 +92,11 @@ export const Details = () => {
                   </figcaption>
                 </figure>
               </section>
-              </div>
-            </>
-            : null }
-      </section>
-      {ModalData ? (
-          <Modal />
+            </div>
+          </>
         ) : null}
-      </>
-        )
-    
-}
-                          
+      </section>
+      {ModalData ? <Modal home_id={home_id} /> : null}
+    </>
+  );
+};
--- src/Components/Partials/Modal.jsx
+++ src/Components/Partials/Modal.jsx
@@ -1,19 +1,49 @@
-import { useContext } from "react"
-import { ModalContent } from "../StateManagement/ModalData"
-import Style from '../../Assets/scss/Modal.module.scss'
+import { useContext, useEffect, useState } from "react";
+import { ModalContent } from "../StateManagement/ModalData";
+import Style from "../../Assets/scss/Modal.module.scss";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import axios from "axios";
 
+export const Modal = ({ home_id }) => {
+  const { setModalData } = useContext(ModalContent);
+  const [homeDetails, setHomeDetails] = useState();
 
-export const Modal = () => {
-  
-  const { ModalData, setModalData } = useContext(ModalContent)
+  useEffect(() => {
+    const getDetails = async () => {
+      try {
+        const result = await axios.get(
+          `https://api.mediehuset.net/homelands/homes/${home_id}`
+        );
+        setHomeDetails(result.data.item);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getDetails();
+  }, [home_id]);
 
-  const close = () => setModalData(false)
+  console.log(homeDetails);
 
-  console.log(ModalData)
+  const close = () => setModalData(false);
 
   return (
     <div className={Style.wrapper}>
-      <p onClick={close}>X</p>
+      <p onClick={close} className={Style.cross}>
+        X
+      </p>
+      <div className={Style.sliderWrapper}>
+        <Carousel>
+          {homeDetails &&
+            homeDetails.images.map((item) => {
+              return (
+                <div key={item.id}>
+                  <img src={item.filename.large} />
+                </div>
+              );
+            })}
+        </Carousel>
+      </div>
     </div>
-  )
-}
+  );
+};
